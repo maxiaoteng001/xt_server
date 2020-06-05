a681a4e9b3d9bd5ec8e778a1e1ed0e44e110de66
maxiaoteng <jizhuwo00@hotmail.com>
2020-06-05 11:52:33 +0800

    自动部署脚本

---
+++ start_server.sh
@@ -0,0 +1,17 @@
+echo 'git 开始拉取'
+git pull
+echo 'git 拉取结束'
+
+echo '开启虚拟环境'
+source venv/bin/activate
+
+echo '杀掉之前进程'
+ps -efww|grep -w 'gevent_run.py'|grep -v grep|cut -c 9-15|xargs kill -9
+
+echo '启动服务'
+nohup python3 gevent_run.py >nohup.server 2>&1 &
+echo '启动服务完成'
+
+deactivate
+echo '退出虚拟环境'
+
