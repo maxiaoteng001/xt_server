f612545a64cf8bee588567b2ea5e37df8b044601
maxiaoteng <jizhuwo00@hotmail.com>
2020-06-22 16:54:36 +0800

    调整密码的复杂度

--- src/tools/password.py
+++ src/tools/password.py
@@ -2,11 +2,17 @@ import string
 import random
 import logging
 import json
+from flask import request
 from src.tools import tools_bp
 
 
 @tools_bp.route('/random_string', methods=['GET'])
 def random_string():
-    str = ''.join([random.choice(string.ascii_letters + string.digits + string.punctuation) for _ in range(20)])
+    # 默认密码只包含字母和数字
+    level = request.args.get('level', 4)
+    if level == 4:
+        str = ''.join([random.choice(string.ascii_letters + string.digits) for _ in range(20)])
+    elif level == 5:
+        str = ''.join([random.choice(string.ascii_letters + string.digits + string.punctuation) for _ in range(20)])
     logging.info('生成随机密码:{}'.format(str))
     return json.dumps({'random_string': str})
